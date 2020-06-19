4c456d6c2a18eb6697c96eaf5328d3494e209d53
AakanshaMangla <52341767+AakanshaMangla@users.noreply.github.com>
2020-06-19 15:01:47 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+//optional to write file extension, you can simply write heading
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/components/list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Manchurian</li>
+      <li>White Saus Pasta</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
